9f20841ce5e507d44a62dbb22e6ddadaac814709
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2016-07-22 13:22:58 +0200

    Bugfix: allocated memory was too small.

--- NVRAMaccess_beatnik.c
+++ NVRAMaccess_beatnik.c
@@ -2,9 +2,9 @@
  *
  * Project:        RTEMS-GeSys
  * Module:         GEV boot parameters access
- * File:           NVRAMaccess_mvme5500.c
+ * File:           NVRAMaccess_beatnik.c
  *
- * Description:    Support for motLoad GEV access on MVME5500
+ * Description:    Support for motLoad GEV access on MVME5500/MVME6100
  *
  * Author(s):      Dan Eichel
  *
@@ -17,7 +17,7 @@
 /* values for MVME5500 board */
 #define GEV_START        (0xf1100000 + 0x10000 + 0x70F8)
 #define GEV_SIZE         3592
-#define MOTSCRIPT_PART1  "dla=malloc 0x280000\ntftpGet -d/dev/"
+#define MOTSCRIPT_PART1  "dla=malloc 3000000\ntftpGet -d/dev/"
 #define MOTSCRIPT_PART2  " -adla\nnetShut\ngo -adla"
 
 #include "NVRAMaccess_GEV.c"
--- NVRAMaccess_mvme5500.c
+++ NVRAMaccess_mvme5500.c
@@ -17,7 +17,7 @@
 /* values for MVME5500 board */
 #define GEV_START        (0xf1100000 + 0x10000 + 0x70F8)
 #define GEV_SIZE         3592
-#define MOTSCRIPT_PART1  "dla=malloc 0x280000\ntftpGet -d/dev/"
+#define MOTSCRIPT_PART1  "dla=malloc 3000000\ntftpGet -d/dev/"
 #define MOTSCRIPT_PART2  " -adla\nnetShut\ngo -adla"
 
 #include "NVRAMaccess_GEV.c"
