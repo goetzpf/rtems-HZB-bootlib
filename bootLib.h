c9062be814dcd7eda53fab471d2fb374c309b707
Benjamin Franksen <Ben Franksen benjamin.franksen@helmholtz-berlin.de>
2018-04-16 17:37:52 +0200

    remove param_lengths and param_offsets from bootLib.h

--- bootLib.h
+++ bootLib.h
@@ -17,87 +17,44 @@
 #ifndef BOOTLIB_H
 #define BOOTLIB_H
 
-#include <netinet/in.h>             /* uint32_t */
+#include <netinet/in.h>                 /* uint32_t */
 
+#define BOOT_DEV_LEN            20      /* max chars in device name */
+#define BOOT_HOST_LEN           20      /* max chars in host name */
+#define BOOT_ADDR_LEN           30      /* max chars in net addr */
+#define BOOT_TARGET_ADDR_LEN    50      /* IP address + mask + lease times */
+#define BOOT_ADDR_LEN           30      /* max chars in net addr */
+#define BOOT_FILE_LEN           80      /* max chars in file name */
+#define BOOT_USR_LEN            20      /* max chars in user name */
+#define BOOT_PASSWORD_LEN       20      /* max chars in password */
+#define BOOT_OTHER_LEN          80      /* max chars in "other" field */
+#define BOOT_FIELD_LEN          80      /* max chars in boot field */
 
-#define BOOT_DEV_LEN            20  /* max chars in device name */
-#define BOOT_HOST_LEN           20  /* max chars in host name */
-#define BOOT_ADDR_LEN           30  /* max chars in net addr */
-#define BOOT_TARGET_ADDR_LEN    50  /* IP address + mask + lease times */
-#define BOOT_ADDR_LEN           30  /* max chars in net addr */
-#define BOOT_FILE_LEN           80  /* max chars in file name */
-#define BOOT_USR_LEN            20  /* max chars in user name */
-#define BOOT_PASSWORD_LEN       20  /* max chars in password */
-#define BOOT_OTHER_LEN          80  /* max chars in "other" field */
-#define BOOT_FIELD_LEN          80  /* max chars in boot field */
-
-#define MAX_LINES 15
-
-
-typedef struct                      /* BOOT_PARAMS */
-{
-  char bootDev[BOOT_DEV_LEN];       /* boot device code */
-  char hostName[BOOT_HOST_LEN];     /* name of host */
-  char targetName[BOOT_HOST_LEN];   /* name of target */
-  char ead[BOOT_TARGET_ADDR_LEN];   /* ethernet internet addr */
-  char bad[BOOT_TARGET_ADDR_LEN];   /* backplane internet addr */
-  char had[BOOT_ADDR_LEN];          /* host internet addr */
-  char gad[BOOT_ADDR_LEN];          /* gateway internet addr */
-  char bootFile[BOOT_FILE_LEN];     /* name of boot file */
-  char startupScript[BOOT_FILE_LEN];/* name of startup script file */
-  char usr[BOOT_USR_LEN];           /* user name */
-  char passwd[BOOT_PASSWORD_LEN];   /* password */
-  char other[BOOT_OTHER_LEN];       /* available for applications */
-  int procNum;                      /* processor number */
-  int flags;                        /* configuration flags */
-  int unitNum;                      /* network device unit number */
+typedef struct {                        /* BOOT_PARAMS */
+    char bootDev[BOOT_DEV_LEN];         /* boot device code */
+    char hostName[BOOT_HOST_LEN];       /* name of host */
+    char targetName[BOOT_HOST_LEN];     /* name of target */
+    char ead[BOOT_TARGET_ADDR_LEN];     /* ethernet internet addr */
+    char bad[BOOT_TARGET_ADDR_LEN];     /* backplane internet addr */
+    char had[BOOT_ADDR_LEN];            /* host internet addr */
+    char gad[BOOT_ADDR_LEN];            /* gateway internet addr */
+    char bootFile[BOOT_FILE_LEN];       /* name of boot file */
+    char startupScript[BOOT_FILE_LEN];  /* name of startup script file */
+    char usr[BOOT_USR_LEN];             /* user name */
+    char passwd[BOOT_PASSWORD_LEN];     /* password */
+    char other[BOOT_OTHER_LEN];         /* available for applications */
+    int procNum;                        /* processor number */
+    int flags;                          /* configuration flags */
+    int unitNum;                        /* network device unit number */
 } BOOT_PARAMS;
-
-
-static const size_t param_lengths[] = {
-  BOOT_DEV_LEN,
-  BOOT_HOST_LEN,
-  BOOT_HOST_LEN,
-  BOOT_TARGET_ADDR_LEN,
-  BOOT_TARGET_ADDR_LEN,
-  BOOT_ADDR_LEN,
-  BOOT_ADDR_LEN,
-  64,                               /* ppcBug allows only 64 characters for boot file */
-  BOOT_FILE_LEN,
-  BOOT_USR_LEN,
-  BOOT_PASSWORD_LEN,
-  BOOT_OTHER_LEN,
-  4,
-  4,
-  4
-};
-
-
-static const size_t param_offsets[] = {
-  offsetof (BOOT_PARAMS, bootDev),
-  offsetof (BOOT_PARAMS, hostName),
-  offsetof (BOOT_PARAMS, targetName),
-  offsetof (BOOT_PARAMS, ead),
-  offsetof (BOOT_PARAMS, bad),
-  offsetof (BOOT_PARAMS, had),
-  offsetof (BOOT_PARAMS, gad),
-  offsetof (BOOT_PARAMS, bootFile),
-  offsetof (BOOT_PARAMS, startupScript),
-  offsetof (BOOT_PARAMS, usr),
-  offsetof (BOOT_PARAMS, passwd),
-  offsetof (BOOT_PARAMS, other),
-  offsetof (BOOT_PARAMS, procNum),
-  offsetof (BOOT_PARAMS, flags),
-  offsetof (BOOT_PARAMS, unitNum)
-};
 
 
 /*+**************************************************************************
  *
  * Variable:    BOOT_LINE_ADRS
  *
- * Description: vxWorks bootLib API - under vxWorks points this variable
- *              into the NVRAM location where boot params stored
+ * Description: vxWorks bootLib API - pointer to the NVRAM location where
+ *              boot params are stored in vxWorks
  *
  **************************************************************************-*/
 extern char *BOOT_LINE_ADRS;
@@ -134,6 +91,5 @@ extern int bootStructToString (char *paramString, BOOT_PARAMS * pBootParams);
  *
  **************************************************************************-*/
 extern char *bootStringToStruct (char *bootString, BOOT_PARAMS * pBootParams);
-
 
 #endif /* BOOTLIB_H */
